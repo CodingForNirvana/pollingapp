77b5a716284c7daced49c3b6843c4e395c13ba1e
Amol Chaudhari <amolchaudhari@Amols-Mac-Pro.home>
2014-11-16 02:50:41 -0500

    Working collectionview. Problem was autolayout

---
+++ pollingapp/PollsCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PollsCollectionViewCell.swift
+//  pollingapp
+//
+//  Created by Amol Chaudhari on 11/16/14.
+//  Copyright (c) 2014 MadBullTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class PollsCollectionViewCell: UICollectionViewCell {
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+}
--- pollingapp/PollsViewController.swift
+++ pollingapp/PollsViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 
-class PollsViewController: UIViewController {
+class PollsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    @IBOutlet weak var collectionView: UICollectionView!
     
     @IBAction func openMenuTapped(AnyObject) {
         
@@ -23,11 +24,33 @@ class PollsViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 90)
+
+   //     collectionView!.registerClass(PollsCollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        return cell
+    }
+
     
 }
